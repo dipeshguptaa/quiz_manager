34bfb3bbf2a651e2541b38d315d31f81129d7c3d
Dipesh Gupta <emailfordipesh@gmail.com>
2025-12-20 01:17:38 +0530

    Fix Logger load order for Rails 6 on Render

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,5 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require 'logger'
-require 'bundler/setup'
-require 'bootsnap/setup'
+require "bundler/setup"
+require "bootsnap/setup"
+require "logger"
